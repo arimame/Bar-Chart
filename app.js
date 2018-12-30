e171ba764ee2f16e00e65f91cb8373508ff2751c
Alissa Balge <alissa.balge@gmail.com>
2018-12-30 00:07:02 +0000

    made scaling function, made y axis html and css

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
+
+const colors= ['aqua', 'blue', 'fuchsia', 'green',
+'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
+'silver', 'teal', 'yellow'];
+
+  function scaling(data) {
+      const digits = data.toString().length;
+      const pixels = Math.pow(10, (3 - digits));
+      return pixels;
+  }
+
   function drawBarChart(data) {
     const width = data.length;
     for (var i = 0; i < width; i++) {
-      let height = data[i] * 10
-      $('#container').append(`<div style="background-color: blue; width: 50px; height: ${height}px; margin-right: 10px"></div>`)
+      let height = data[i] * (scaling(data[i]))/2;
+      console.log(height);
+      $('#container').append(`<div style="background-color: ${colors[i]}; width: 50px; height: ${height}px; margin-right: 10px"></div>`)
     }
   }
-  drawBarChart([1,2,4, 10, 50, 100]);
+
+  drawBarChart([10, 20, 40, 75]);
 });

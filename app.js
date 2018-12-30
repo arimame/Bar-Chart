e64aa719ae4740a86369dbad4ae13335879e6042
Alissa Balge <alissa.balge@gmail.com>
2018-12-30 21:28:44 +0000

    added functions for x axis and y axis

--- app.js
+++ app.js
@@ -10,14 +10,34 @@ const colors= ['aqua', 'blue', 'fuchsia', 'green',
       return pixels;
   }
 
+  function yaxis(data) {
+    let zero = "";
+    for (var i = 0; i < (data - 1); i++) {
+      zero += "0";
+    }
+    for (var i = 10; i >= 1; i--) {
+      $('#y-axis').append(`<div class="number">${i + zero}</div>`)
+    }
+  }
+
+  function xaxis(data) {
+    for (var i = 0; i < data.length; i++) {
+      $('#x-axis').append(`<div style="width: 50px; margin-right: 10px; text-align: center">${data[i].label}</div>`)
+    }
+
+  }
+
   function drawBarChart(data) {
     const width = data.length;
     for (var i = 0; i < width; i++) {
-      let height = data[i] * (scaling(data[i]))/2;
+      let height = data[i].value * (scaling(data[i].value))/2;
       console.log(height);
       $('#container').append(`<div style="background-color: ${colors[i]}; width: 50px; height: ${height}px; margin-right: 10px"></div>`)
     }
   }
 
-  drawBarChart([10, 20, 40, 75]);
+  const chartData = [{value: 1000, label: "cats"}, {value: 2000, label: "dogs"}, {value: 4000, label: "birds"}, {value: 7500, label: "sharks"}];
+  yaxis(4);
+  xaxis(chartData)
+  drawBarChart(chartData);
 });
